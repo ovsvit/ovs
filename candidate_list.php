0f10205e85a72438e18eae59565781d00ddefd0c
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-14 13:35:08 +0530

    Add files via upload

---
+++ candidate_list.php
@@ -0,0 +1,135 @@
+<?php
+$con=mysqli_connect("localhost","root","","ovs") or die(mysqli_error($con));
+$query="select party_name,party_description,party_symbol,Candidate_ID,candidate_pic from register_candidate";
+$result=mysqli_query($con,$query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Candidate_List</title>
+    <style>
+body
+{
+  background-image: url("img1.jpg");
+}
+.head li,h1,ul{
+            display: inline;
+        }
+li {
+            font-size: 1.5em;
+        }
+
+nav {
+            border-style: double;
+            border-color: black;
+            color: black;
+            background-color: rgb(6, 224, 240);
+            padding: 5px;
+            border-radius: 10px;   
+        }
+ .head_nav {
+            margin-left: 80%;
+        }
+table,tr,td,th
+{
+    border:2px solid black;
+    border-collapse:collapse;
+   
+
+}
+
+table{
+    margin-top:60px;
+    border-collapse:collapse;
+    width:100%;
+    padding:3px;
+    height:100px;
+    text-align: center;
+    background-color: rgb(6, 224, 240);
+    column-width:auto;
+}
+th{
+    color:black;
+    style:solid;
+    background-color:blue;
+    font-size:1.2em;
+}
+td{
+    vertical-align: middle;
+}
+.h{
+    color:black;
+    width:300px;
+    height:30px;
+    background-color: aqua;
+    border:2px solid ;
+    padding:5px;
+    border-radius:10px;
+    background-color: rgb(6, 224, 240);
+    text-align:center;
+    margin-top:50px;
+
+}
+
+</style>
+</head>
+<body>
+<div class="head">
+        <nav>
+            <h1> Online Voting System</h1>
+            <ul class="head_nav">
+                <li class="head_list"><a href="home.html">Home</a></li>
+                <li>|</li>
+                <li><a href="logout.html">Logout</a></li>
+            </ul>    
+        </nav>
+    </div>
+    <center>
+    <div class="h">
+        <h1>Candidate List</h1>
+    </div>
+    </center>
+    
+    <table>
+        <tr>
+           
+            <th>Party_Name</th>
+            <th>Party_Description</th>
+            <th>Party_Symbol</th>
+            <th>Candidate_ID</th>
+            <th>Candidate_pic</th>
+           
+        </tr>
+                                <?php
+                                while($row=mysqli_fetch_assoc($result))
+                                {
+                                ?>
+                            
+                                <td><?php echo $row['party_name'] ?></td>
+                                <td><?php echo $row['party_description'] ?></td>
+                                
+                                <td><img src="data:image/jpeg;base64,
+                                <?php echo base64_encode($row['party_symbol']); ?>"
+                               alt="Candidate Pic" width="100" height="100"></td>
+                               
+                                <td><?php echo $row['Candidate_ID'] ?></td>
+                                
+                                 <td><img src="data:image/jpeg;base64,
+                                <?php echo base64_encode($row['candidate_pic']); ?>"
+                               alt="Candidate Pic" width="100" height="100"></td>
+           
+
+                              
+                            </tr>
+                                 <?php   
+                                }
+                                ?>
+                            
+                        </table>
+                    </div>
+</table>
+</body>
+</html>

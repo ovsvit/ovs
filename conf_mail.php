bc013ad9475dd86cabd7d2966ac0d0bf89b8d872
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-14 13:59:28 +0530

    Add files via upload

---
+++ conf_mail.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>Send Email</title>
+    <style>
+        body{
+                   
+            background-image:url(img1.jpg);
+        }
+        .field{
+            width:400px;
+            margin:auto;
+            margin-top: 40px;
+            margin-bottom: 40px;
+            background-color:rgb(6, 224, 240);
+            color:black;
+            padding:10px 20px 30px;
+            font-size:1.1em;  
+            border-radius:10px;         
+        
+        
+        }
+        input[type=submit], input[type=button] {
+                    background-color: navy;
+                    border: none;
+                    color: white;
+                    padding: 7px 14px;
+                    text-decoration: none;
+                    margin: 2px 1px;
+                    cursor: pointer;
+                   }
+        .label{
+        padding: 5px;
+        }
+        h2{
+            color:black;
+            text-align: center;
+        }
+        
+        div{
+            margin:8px;
+          
+        }
+        
+        .inp{
+            margin-left: 5px;
+            width:99%;
+            font-size:80%;
+            border: 1px solid blue;
+            padding:8px 10px;
+            border-radius: 7px;
+            display:block;
+            box-shadow:5px;
+            box-sizing: border-box;
+        }
+        .btn{
+            text-align: center;
+           padding:10px;
+         
+        }
+        button{
+            text-align:center;
+            border-radius:1px;
+            padding:0px;
+            
+        }
+        nav{
+            border-style:double;
+            border-color:black;
+            color:black;
+            background-color:rgb(6, 224, 240);
+            padding:5px;
+            border-radius:10px;   
+        
+        }
+        ul{
+            margin-left:80%;
+        }
+        
+        li,h1{
+            display:Inline;
+        }
+        li{
+            font-size:1.5em;
+        }
+        .bot{
+                color:black;
+                padding: 10px;
+            }
+      
+        </style>
+                
+
+</head>
+    <body>
+
+
+    <div class="head">
+        <nav>
+            <h1> Online Voting System</h1>
+            
+            <ul >
+                <li ><a href="home.html">Home</a></li>
+                <li>|</li>
+                <li><a href="logout.html">Logout</a></li>
+            </ul>    
+        </nav>
+        
+         
+    </div>
+    
+    <fieldset class="field">
+        <b>
+            <legend> <h2 text-align: center>Confirmation </h2></legend>
+               
+            
+        </b>
+
+        <form class="form" action="send.php" method="post">
+            <div>
+                <label class="label">Email:</label>
+                <input class="inp" type="email" name="email" value="" placeholder="Enter your email id">
+            </div>
+        <!--  Email <input type="email" name="email" value=""><br>
+          Subject <input type="text" name="subject" value=""><br>
+          Message <input type="text" name="message" value=""><br>
+       
+        <center><button type="submit" name="send" >Send<br></center>-->
+        <center><input type="submit" name="send" ></center>
+
+          </form>
+
+    </fieldset>
+
+
+          
+
+    </body>
+</html>
---
+++ send.php
@@ -0,0 +1,47 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $mail=new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host='smtp.gmail.com';
+    $mail->SMTPAuth=true;
+    $mail->Username='jumanabegum.m2021@vitstudent.ac.in';
+    $mail->Password='lxbkooykizcjkgbs';
+    $mail->SMTPSecure='ssl';
+    $mail->Port=465;
+
+    $mail->setFrom('jumanabegum.m2021@vitstudent.ac.in');
+
+    $mail->addAddress($_POST["email"]);
+
+    $mail->isHTML(true);
+
+    //$mail->Subject=$_POST["subject"];
+    //$mail->Body=$_POST["message"];
+
+    // Set the subject from the form or a default value
+       $mail->Subject = isset($_POST["subject"]) ? $_POST["subject"] : 'Confirmation';
+
+    // Set the message body from the form or a default value
+      $mail->Body = !empty($_POST["message"]) ? $_POST["message"] : 'Voted successfully';
+
+    $mail->send();
+
+    echo
+    "
+    <script>
+    alert('Confirmation mail has been Successfully sent');
+    document.location.href='conf_mail.php';
+    </script>
+    ";
+
+}
+
+?>

81d2c2e536090dc950c4acffba62f179e49c2044
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-16 16:39:26 +0530

    Update ressuc.php

--- ressuc.php
+++ ressuc.php
@@ -4,7 +4,7 @@ include("db.php");
 
 if($_SERVER['REQUEST_METHOD']=='POST')
 $dt=$_POST['dt'];
-$query="update admin_login set set_time where username ='admin' and password='admin'";
+$query="update admin_login set set_time='$dt' where username ='admin' and password='admin'";
 $result=mysqli_query($conn,$query);
 
 header("location: adminhome.php");

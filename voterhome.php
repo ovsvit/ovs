73b8c658aa8cfb495528b784661989c8a64d60a1
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-21 03:37:52 -0800

    Update and rename voterhome.html to voterhome.php

--- voterhome.php
+++ voterhome.php
@@ -1,3 +1,17 @@
+<?php
+session_start(); 
+if (isset($_GET['username'])) {
+   
+    $usernameFromURL = $_GET['username'];
+
+    echo "Welcome, $usernameFromURL!";
+} else {
+  
+    echo "Username not found in the URL.";
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,14 +41,14 @@
             <div class ="profile">
                 <div class="content1">
                     <img src="profile.png" alt="profile">
-                    <a href="voterviewprofile.php"><h3 id="info1"><button class="btn">My Profile</button></h3></a>
+                    <a href="getdetails.php?username=<?php echo urlencode($usernameFromURL); ?>"><h3 id="info1"><button class="btn">My Profile</button></h3></a>
                 </div>
                 
             </div>
             <div class ="vast">
                 <div class="content2">
                     <img src="vote.png" alt="vote">
-                    <a href="votecast.php"><h3  id="info2"><button class="btn">Vote Cast</button></h3></a>
+                    <a href="votecast.php?username=<?php echo urlencode($usernameFromURL); ?>"><h3  id="info2"><button class="btn">Vote Cast</button></h3></a>
                 </div>
                 
             </div>
@@ -42,14 +56,14 @@
             <div class ="update">
                 <div class="content3">
                     <img src="update.png" alt="update">
-                    <a href="update_voter_password.php"><h3  id="info3"><button class="btn">Update Credentials</button></h3></a>
+                    <a href="update_voter_password.php?username=<?php echo urlencode($usernameFromURL); ?>"><h3  id="info3"><button class="btn">Update Credentials</button></h3></a>
                 </div>
                 
             </div>
             <div class ="candidate">
                 <div class="content4">
                     <img src="cand.png" alt="candidate">
-                    <a href="candidate_list.php"><h3  id="info4"><button class="btn">Candidates List</button></h3></a>
+                    <a href="candidate_list.php?username=<?php echo urlencode($usernameFromURL); ?>"><h3  id="info4"><button class="btn">Candidates List</button></h3></a>
                 </div>
                 
             </div>

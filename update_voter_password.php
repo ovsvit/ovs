244b5c630e1dcd9039627d37bb4bb71d062b117d
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-21 09:58:48 +0530

    Update update_voter_password.php
    home added

--- update_voter_password.php
+++ update_voter_password.php
@@ -129,9 +129,9 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
             <h1> Online Voting System</h1>
             
             <ul >
-                <li ><a href="home.html">Home</a></li>
+                <li ><a href="voterhome.html">Home</a></li>
                 <li>|</li>
-                <li><a href="logout.html">Logout</a></li>
+                <li><a href="home1.html">Logout</a></li>
             </ul>    
         </nav>
         

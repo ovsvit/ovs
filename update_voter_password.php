562c9fcd017cafc861519b5de5dbaf93de176f6a
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 16:51:14 +0530

    Update update_voter_password.php

--- update_voter_password.php
+++ update_voter_password.php
@@ -21,7 +21,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
  mysqli_query($conn,$query);
 }
 
-
+$home="homepage";
 
 
 ?>
@@ -129,7 +129,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
             <h1> Online Voting System</h1>
             
             <ul >
-                <li ><a  href="voterhome.php?username=<?php echo urlencode($u)?>">Home</a></li>
+                <li ><a  href="voterhome.php?username=<?php echo urlencode($home)?>">Home</a></li>
                 <li>|</li>
                 <li><a href="home1.html">Logout</a></li>
             </ul>    

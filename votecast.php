3d85c3c2781e0e7ce3c04d05a6accb9984aa85fb
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 16:26:41 +0530

    Update votecast.php

--- votecast.php
+++ votecast.php
@@ -90,7 +90,7 @@ td{
         <nav>
             <h1> Online Voting System</h1>
             <ul class="head_nav">
-                <li class="head_list"><a  href="voterhome.php?username=<?php echo urlencode($cid)?>">Home</a></li>
+                <li class="head_list"><a  href="voterhome.php?username=<?php echo urlencode(null)?>">Home</a></li>
                 <li>|</li>
                 <li><a href="home1.html">Logout</a></li>
             </ul>    

94040201df22af891b826c990c20a56d9040d238
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 00:18:33 -0800

    Update candidatelogin.php

--- candidatelogin.php
+++ candidatelogin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voter login</title>
+    <title>Candidate login</title>
     <style>
         body
 {

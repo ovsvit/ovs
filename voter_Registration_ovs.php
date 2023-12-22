9a62695722dde2963af867cbba2531c23de064bd
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 17:15:58 +0530

    Update voter_Registration_ovs.php

--- voter_Registration_ovs.php
+++ voter_Registration_ovs.php
@@ -185,7 +185,7 @@ body
         
         <h4 id="home">
           Go to
-          <a  href="voterhome.php?username=<?php echo urlencode($voterid)?>"
+          <a  href="voterhome.php?username=<?php echo urlencode($home)?>"
             
             target="-blank"
             >Home</a

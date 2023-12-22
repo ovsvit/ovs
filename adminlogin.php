5cefa6e541e8b0d9f086157caea5c2b1aeb6d0e9
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 15:31:30 +0530

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voter login</title>
+    <title>Admin login</title>
     <style>
         body
 {

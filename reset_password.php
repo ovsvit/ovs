3766643585225e78874f5a26cb85513756f4e623
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 16:50:42 +0530

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -33,7 +33,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
-    <title>Update password</title>
+    <title>Reset password</title>
     <style>
         body{
                    
@@ -141,7 +141,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
    
     <fieldset class="field">
         <b>
-            <legend> <h2 text-align: center>Update password </h2></legend>
+            <legend> <h2 text-align: center>Reset password </h2></legend>
                
             
         </b>

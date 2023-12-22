c7a91096a94af2ad3bfad8c1a9127609ae6e54a7
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 16:50:14 +0530

    Update reset_password_cand.php

--- reset_password_cand.php
+++ reset_password_cand.php
@@ -33,7 +33,7 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
-    <title>Update password</title>
+    <title>Reset password</title>
     <style>
         body{
                    

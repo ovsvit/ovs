b05e7cc874c422e4cb0fd8305e843aa1b7f16d43
ovsvit <153751218+ovsvit@users.noreply.github.com>
2023-12-22 00:26:23 -0800

    Update candidatelogin.php

--- candidatelogin.php
+++ candidatelogin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cndidate login</title>
+    <title>Candidate login</title>
     <style>
         body
 {

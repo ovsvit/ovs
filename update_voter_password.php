051c70c75be055346c7a30f563ee4d60e8c004f6
ovsvit <153751218+ovsvit@users.noreply.github.com>
2024-01-21 21:21:02 +0530

    Update update_voter_password.php

--- update_voter_password.php
+++ update_voter_password.php
@@ -11,34 +11,157 @@ if($_SERVER['REQUEST_METHOD']== 'POST')
     $o=$_POST['o'];
     $b=$_POST['b'];
 
-   /*$query = "UPDATE updpass SET Password=? WHERE Username=?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "ss", $u, $a);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);*/
-
  $query="UPDATE voter_login SET password='$a' where username='$u'";
- mysqli_query($conn,$query);
+ //mysqli_query($conn,$query);
+ $query_run=mysqli_query($conn,$query);
+  if($query_run)
+  {
+    header("Location: voterlogin.php");
+  }
 }
 
 $home="homepage";
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <title>Update password</title>
+    <title>Registration successful</title>
     <style>
-        body{
-                   
-            background-image:url(img1.jpg);
-        }
-        .field{
+body
+{
+  background-image: url("background3.jpeg");
+}
+.msg
+{
+height:70px;
+  width: 600px;
+  margin: 10vh auto 0 auto;
+  background-color: rgb(98, 230, 240);
+  border-radius: 20px;
+  padding: 5px;
+  margin-top:20px;
+  border:2px solid darkblue;
+ 
+  
+}
+#h
+{
+    
+    animation:blow 3s infinite linear;
+}
+@keyframes blow
+{
+
+    0%
+    {
+        transform:translateY(0) scale(1);
+    }
+    50%
+    {
+        transform:translateY(-10px) scale(1.05);
+    }
+}
+.pw
+{
+  height:500px;
+  width: 500px;
+  margin: 10vh auto 0 auto;
+  background-color:rgb(6, 224, 240);
+  border-radius: 10px;
+  padding: 10px;
+  margin-top:20px;
+  border-style:double;
+            border-color:black;
+}
+#pw
+{
+  
+  padding:5px;
+  border-radius:5px;
+  height:500px;
+  width:400px;
+  margin-left:30px;
+  color:black;
+  margin-bottom:5px;
+}
+
+#repw
+{
+  
+  padding:5px;
+  border-radius:5px;
+  height:20px;
+  width:400px;
+  margin-left:30px;
+  color:black;
+  margin-bottom:5px;
+}
+
+#voterid
+{
+  
+  padding:5px;
+  border-radius:5px;
+  height:20px;
+  width:400px;
+  margin-left:30px;
+  color:black;
+  margin-bottom:5px;
+}
+
+
+#btn
+{
+   
+  background-color: blue;
+  color: white;
+  border: 1px solid;
+  padding: 7px;
+  margin: 10px 0px;
+  cursor: pointer;
+  border-radius: 5px;
+  font-size: 20px;
+  width: 50%;
+  margin-left: 100px;
+  margin-top:10px;
+  
+  margin-bottom:50px;
+}
+.note
+{
+
+    height:200px;
+  width:600px;
+  background-color: rgb(23, 151, 194);
+  color:black;
+  border: 2px solid;
+  margin-right:50%;
+}
+
+.inputgroup.error #pw1 {
+  
+  color: rgb(238, 25, 25);
+  margin-left: 30px;
+}
+
+.inputgroup.success input {
+    
+    border:2px solid rgb(11, 197, 11)
+  }
+  .inputgroup.error input {
+    
+    border:2px solid red;
+  }
+
+  #password
+  {
+    margin-bottom:40px;
+  }
+  .field{
             width:400px;
             margin:auto;
             margin-top: 40px;
@@ -118,18 +241,17 @@ $home="homepage";
                 color:black;
                 padding: 10px;
             }
-      
-        </style>
-                
 
+    </style>
+    
 </head>
 <body>
-    <div class="head">
+<div class="head">
         <nav>
             <h1> Online Voting System</h1>
             
             <ul >
-                <li ><a  href="voterhome.php?username=<?php echo urlencode($home)?>">Home</a></li>
+                <li ><a href="candidatehomepage.php?username=". urlencode($u)>Home</a></li>
                 <li>|</li>
                 <li><a href="home1.html">Logout</a></li>
             </ul>    
@@ -137,31 +259,31 @@ $home="homepage";
         
          
     </div>
-   
-    <fieldset class="field">
-        <b>
-            <legend> <h2 text-align: center>Update password </h2></legend>
-               
-            
-        </b>
-        
-        <form method="POST" class="form">
-            <div>
+  <form  method="POST" id="form">
+    
+    <div class="pw">
+       
+        <center><h2 id="password">Create Your Password</h2></center>
+        <div class="inputgroup">
                 <label class="label">Username:</label>
                 <input class="inp" type="text" id="u" name="u" placeholder="Enter your username" >
+                <div class="error" id="pw1"></div>
             </div>
-            <div>
+            <div class="inputgroup">
                 <label class="label">Old password:</label>
                 <input class="inp" type="text" id="o" name="o" placeholder="Enter old password" >
+                <div class="error" id="pw1"></div>
             </div>
-            <div>
+            <div class="inputgroup">
                 <label class="label">New password:</label>
                 <input  class="inp" type="password" id="a" name="a" placeholder="Enter new password" >
+                <div class="error" id="pw1"></div>
                 
             </div>
-            <div>
+            <div class="inputgroup">
                 <label class="label">Confirm password:</label>
                 <input  class="inp" type="password" id="b" name="b" placeholder="Enter confirm password">
+                <div class="error" id="pw1"></div>
                 
             </div>
             <input type="checkbox" onclick="myFunction()">Show Password
@@ -169,14 +291,15 @@ $home="homepage";
                 <p id="message"></p>
                 <p id="show"></p>
             </div>
-            <div class="btn">
-               <center><input type="submit" onclick="matchPassword()" value="submit"></center>
-              <!-- <button onclick="matchPassword()">submit</button>-->
-            </div>
-            
-            
-        </form>
-    </fieldset>
+        
+      
+        <button id="btn" type="submit">Update Password</button>
+        <br>
+        
+        <br><br>
+        
+
+    </div>
     <div class="head">
         <nav>
         <h3 style="color:Black"><u>Note:</u></h3>
@@ -188,124 +311,105 @@ $home="homepage";
         
     </nav>
     </div>
-    
-    
+  
 </body>
+</form>
 <script>
-    function myFunction() {
-  var x = document.getElementById("a");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
+
+const form = document.getElementById("form");
+const a= document.getElementById("a");
+const b= document.getElementById("b");
+const u= document.getElementById("u");
+ form.addEventListener("submit", (e) => {
+  if (!validateInputs()) {
+    e.preventDefault();
   }
-  var y = document.getElementById("b");
-  if (y.type === "password") {
-    y.type = "text";
-  } else {
-    y.type = "password";
+  else{
+    alert("Password was created successfully");
   }
+});
+
+ function validateInputs() {
+  let suc = true;
+  const us=u.value.trim();
+  const password = a.value.trim();
+  const repassword = b.value.trim();
+
+
+  if (us === "") {
+    suc = false;
+    seterror(u, "Voterid number is required");
+  } else if (us.length < 10 || us.length > 10) {
+    suc = false;
+    seterror(u, "Voter ID number must be 10 characters");
+  } else {
+    setsuccess(u);
+  }
+
+  if (password === "") {
+    suc = false;
+    seterror(a, "Set your password");
+  }
+  else if(password .length<8 || !checkpass(password) )
+  {
+    suc = false;
+    seterror(a, "Provide a strong password");
+  } 
+  else {
+    setsuccess(a);
+  }
+
+  if (repassword  === "") {
+    suc = false;
+    seterror(b, "Re-enter your password");
+  }
+  else if(password .length<8 || !checkpass(password) )
+  {
+    suc = false;
+    seterror(b, "Provide a strong password");
+  } 
+  else if(password != repassword)
+  {
+    suc = false;
+    seterror(b, "Type the password correctly");
+  } 
+  else {
+    setsuccess(b);
+  }
+  
+  
+
+  return suc;
 }
-    function matchPassword(){
-        let newPass=document.getElementById("a").value;
-        let confPass=document.getElementById("b").value;
-        let oldPass=document.getElementById("o").value;
-       // console.log("Password:",a,'\n',"Confirm Pass");
-        let message=document.getElementById("message");
-    if(oldPass!=newPass)
-    {
-        if(newPass.length>=3)
-        {
-            if(newPass==confPass)
-            {
-            message.textContent="";
-            message.style.backgroundColor="lime";
-              if(newPass.match(/[0-9]/))
-              {
-                message.textContent="";
-                if(newPass.match(/[A-Z]/))
-                {
-                    message.textContent="";
-                    if(newPass.match(/[a-z]/))
-                    {
-                        message.textContent="";
-                        if(newPass.match(/[!\@\#\$\%^\&\*\(\)\-\+\_\=\,\.]/))
-                        {
-                            message.textContent="Password ready to update";
-                            message.style.backgroundColor="lime";
+function checkpass(a)
+{
+  var pass=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
+  if(a.match(pass))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 
-                        }
-                        else{
-                            message.textContent="Your password should contain atleast 1 special character.";
-                            message.style.backgroundColor="red";
-                        }
-                    }
-                    else
-                    {
-                       message.textContent="Your password should contain atleast 1 lowercase letter.";
-                       message.style.backgroundColor="red";
-                    }
-                }
-                else
-                {
-                    message.textContent="Your password should contain atleast 1 uppercase letter.";
-                    message.style.backgroundColor="red";
-                }
-              }
-               else
-              {
-                message.textContent="Your password should contain atleast 1 number";
-                message.style.backgroundColor="red";
-              }
-            }
-            else
-            {
-            message.textContent="Password didn't match";
-            message.style.backgroundColor="red";
-            }
-            
-            
-         
-          
-          
-        }
-        else
-        {
-            message.textContent="Password should contain alteast 8 characters";
-            message.style.backgroundColor="red";
-        }
-     /*  if(a.value.match(/[0-9]/)){
-        message.textContent="containing number";
-           }
-    
-       else{
-              message.textContent="Your password should contain atlest 1 number";
-           }*/
 
-        }
-       
-    
-    else
-    {
-        message.textContent="Your new password should not be your old password";
-    }
-      
-}    
+  
+}
+function seterror(element, msg) {
+  const Inputgroup = element.parentElement;
+  const errorelement = Inputgroup.querySelector(".error");
+  errorelement.innerText = msg;
+  Inputgroup.classList.add("error");
+  Inputgroup.classList.remove("success");
+}
+function setsuccess(element) {
+  const Inputgroup = element.parentElement;
+  const errorelement = Inputgroup.querySelector(".error");
+  errorelement.innerText = "";
+  Inputgroup.classList.add("success");
+  Inputgroup.classList.remove("error");
+}
+
 </script>
 </html>
-
-
-<div class="bd-dark">
-         <div class="container">
-             <div class="row">
-                <div class="col">
-                   <div class="card mt-5">
-                       <div class="card-header">
-
-                       </div>
-                    </div>
-                </div>
-            </div>
-         </div>
-
-     </div>

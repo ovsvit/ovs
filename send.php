acff5ca01fe4b80f24dbd77f29ed5c0af6dfce2b
ovsvit <153751218+ovsvit@users.noreply.github.com>
2024-01-21 18:21:41 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+include("db.php");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -16,31 +18,68 @@ if(isset($_POST["send"])){
     $mail->Password='vozifmqbfpbzunfu';
     $mail->SMTPSecure='ssl';
     $mail->Port=465;
-
-    $mail->setFrom('ovsgproject@gmail.com');
-
-    $mail->addAddress($_POST["email"]);
-
-    $mail->isHTML(true);
-
-    //$mail->Subject=$_POST["subject"];
-    //$mail->Body=$_POST["message"];
-
-    // Set the subject from the form or a default value
-       $mail->Subject = isset($_POST["subject"]) ? $_POST["subject"] : 'Confirmation';
-
-    // Set the message body from the form or a default value
-      $mail->Body = !empty($_POST["message"]) ? $_POST["message"] : 'Voted successfully';
-
-    $mail->send();
-
+    $m=$_POST["email"];
     echo
     "
     <script>
-    alert('Confirmation mail has been Successfully sent');
-    document.location.href='votesuccess.php';
+    alert('$m');
+ 
     </script>
     ";
+
+    $mail->setFrom('ovsgproject@gmail.com');
+    $query="select Email from voter_register where Email='$m'";
+    $res = mysqli_query($conn, $query);
+    if ($res && $res->num_rows > 0) {
+      $row = $res->fetch_assoc();
+      $eml = $row['Email'];
+  } else {
+      echo "<script>alert('Invalid mail id'); document.location.href='conf_mail.php';</script>";
+  }
+  
+    if (!$res) {
+      echo
+      "
+      <script>
+      alert('Invalid mail id');
+      document.location.href='conf_mail.php';
+      </script>
+      ";
+
+    } else {
+        
+     // $row = $result->fetch_assoc();
+     // $row = $result->fetch_assoc();
+if (filter_var($eml, FILTER_VALIDATE_EMAIL)) {
+  $mail->addAddress($eml);
+} else {
+  echo "<script>alert('Invalid email address'); document.location.href='conf_mail.php';</script>";
+}
+
+      //$mail->addAddress($eml);
+
+      $mail->isHTML(true);
+  
+      //$mail->Subject=$_POST["subject"];
+      //$mail->Body=$_POST["message"];
+  
+      // Set the subject from the form or a default value
+         $mail->Subject = isset($_POST["subject"]) ? $_POST["subject"] : 'Confirmation';
+  
+      // Set the message body from the form or a default value
+        $mail->Body = !empty($_POST["message"]) ? $_POST["message"] : 'Voted successfully';
+  
+      $mail->send();
+  
+      echo
+      "
+      <script>
+      alert('Confirmation mail has been Successfully sent');
+      document.location.href='votesuccess.php';
+      </script>
+      ";
+    }
+   
 
 }
 

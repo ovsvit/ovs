22b9db4bb1745ca74200cf4da4937d5559fe31d9
ovsvit <153751218+ovsvit@users.noreply.github.com>
2024-03-01 11:48:58 +0530

    Update canddelete.php

--- canddelete.php
+++ canddelete.php
@@ -5,13 +5,16 @@ if(isset($_GET['id']))
 {
 $id=$_GET['id'];
 }
+$query3="delete from candidate_login where username = (Select Candidateid_number from candidate_register where Serial_num='$id')";
 $query1="delete from candidate_register where Serial_num='$id'";
 $query2="delete from party_register where Serial_num='$id'";
-
+//$query3="delete from candidate_login where username = (Select Candidateid_number from candidate_register where Serial_num='$id')";
+$result3=mysqli_query($conn,$query3);
 $result1=mysqli_query($conn,$query1);
 $result2=mysqli_query($conn,$query2);
 
-if($result1 && $result2)
+
+if($result1 && $result2 && $result3)
 {
   header("Location:managecandidate.php");
 }

86ccf947ef1854cf32e114f8f0603933fe7241ce
ovsvit <153751218+ovsvit@users.noreply.github.com>
2024-03-01 11:50:37 +0530

    Update time.php

--- time.php
+++ time.php
@@ -34,6 +34,7 @@ $currentTimestamp= date('y-m-d H:i:s');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="1">
    
     <title>Update password</title>
     <style>
